7a48d3902a1e0bbfe3452269eadd1bd16cf5ecbc
kunikaesp <kunika@espsofttech.com>
2025-09-12 19:42:17 +0530

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
 import HeroSection from '../components/Sections/HeroSection';
 import TradersSection from '../components/Sections/TradersSection';
 import ProcessSection from '../components/Sections/ProcessSection';
-import ObjectivesSection from '../components/Sections/ObjectivesSection';
 import BenefitsSection from '../components/Sections/BenefitsSection';
-import CustomerSupportSection from '../components/Sections/CustomerSupportSection';
-import TestimonialsSection from '../components/Sections/TestimonialsSection';
 import CallToActionSection from '../components/Sections/CallToActionSection';
 import PricingTable from './pricingTable/PricingTable';
 
